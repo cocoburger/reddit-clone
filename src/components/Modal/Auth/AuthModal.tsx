841d451b00304a282ab22dd91d8a1727c5f4f66f
burger <fsm15@naver.com>
2023-02-01 00:04:34 +0900

    authModal Login Form 추가

--- src/components/Modal/Auth/AuthModal.tsx
+++ src/components/Modal/Auth/AuthModal.tsx
@@ -1,17 +1,16 @@
 import { authModalState } from "@/atoms/authModalAtom";
 import {
-  useDisclosure,
-  Button,
   Modal,
   ModalOverlay,
   ModalContent,
   ModalHeader,
   ModalCloseButton,
   ModalBody,
-  ModalFooter,
+  Flex,
 } from "@chakra-ui/react";
 import React from "react";
 import { useRecoilState } from "recoil";
+import AuthInputs from "./AuthInputs";
 
 const AuthModal: React.FC = () => {
   const [modalState, setModalState] = useRecoilState(authModalState);
@@ -27,10 +26,29 @@ const AuthModal: React.FC = () => {
       <Modal isOpen={modalState.open} onClose={handleClose}>
         <ModalOverlay />
         <ModalContent>
-          <ModalHeader>Modal Title</ModalHeader>
+          <ModalHeader>
+            {modalState.view === "login" && "Login"}
+            {modalState.view === "signup" && "Sign up"}
+            {modalState.view === "resetPassword" && "Reset Password"}
+          </ModalHeader>
           <ModalCloseButton />
-          <ModalBody>
-            <p>모달 테스트</p>
+          <ModalBody
+            display="flex"
+            flexDirection="column"
+            alignItems="center"
+            justifyContent="center"
+          >
+            <Flex
+              direction="column"
+              align="center"
+              justify="center"
+              width="70%"
+              border="1px solid red"
+            >
+              {/* <OAuthButtons */}
+              <AuthInputs />
+              {/* <RestPassword /> */}
+            </Flex>
           </ModalBody>
         </ModalContent>
       </Modal>
---
+++ src/components/Modal/Auth/Login.tsx
@@ -0,0 +1,42 @@
+import { Button, Input } from "@chakra-ui/react";
+import React, { useState } from "react";
+
+type LoginProps = {};
+
+const Login: React.FC<LoginProps> = () => {
+  const [loginForm, setLoginForm] = useState({
+    email: "",
+    password: "",
+  });
+
+  const onSubmit = () => {};
+  const onChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    //update from state
+    setLoginForm((prev) => ({
+      ...prev,
+      [event.target.name]: event.target.value,
+    }));
+  };
+
+  return (
+    <form onSubmit={onSubmit}>
+      <Input
+        name="email"
+        placeholder="email"
+        type="email"
+        mb={2}
+        onChange={onChange}
+      />
+
+      <Input
+        name="password"
+        placeholder="password"
+        type="password"
+        onChange={onChange}
+      />
+      <Button type="submit">로그인</Button>
+    </form>
+  );
+};
+
+export default Login;
