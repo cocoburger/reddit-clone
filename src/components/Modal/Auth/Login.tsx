2282b2f01b1c912f32f0f772bf6f7dd81d9b9b46
burger <fsm15@naver.com>
2023-02-02 23:44:45 +0900

    회원가입 모달창이동 및 CSS 추가

--- src/components/Modal/Auth/Login.tsx
+++ src/components/Modal/Auth/Login.tsx
@@ -1,18 +1,24 @@
-import {Button, Input} from "@chakra-ui/react";
-import React, {useState} from "react";
+import { authModalState } from "@/atoms/authModalAtom";
+import { Button, Flex, Input, Text } from "@chakra-ui/react";
+import React, { useState } from "react";
+import { useSetRecoilState } from "recoil";
 
 type LoginProps = {};
 
 const Login: React.FC<LoginProps> = () => {
+  const setAuthModalState = useSetRecoilState(authModalState);
   const [loginForm, setLoginForm] = useState({
     email: "",
     password: "",
   });
 
+  /**
+   * todo : Firebase logic
+   */
   const onSubmit = () => {};
-    /**
-     * 하나의 함수에서 동적으로 state value 변경
-     */
+  /**
+   * 하나의 함수에서 동적으로 state value 변경
+   */
   const onChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     //update from state
     setLoginForm((prev) => ({
@@ -24,20 +30,70 @@ const Login: React.FC<LoginProps> = () => {
   return (
     <form onSubmit={onSubmit}>
       <Input
+        required
         name="email"
         placeholder="email"
         type="email"
         mb={2}
         onChange={onChange}
+        fontSize="10pt"
+        _placeholder={{ color: "gray.500" }}
+        _hover={{
+          bg: "white",
+          border: "1px solid",
+          borderColor: "blue.500",
+        }}
+        _focus={{
+          outline: "none",
+          bg: "white",
+          border: "1px solid",
+          borderColor: "blue.500",
+        }}
+        bg="gray.50"
       />
-
       <Input
+        required
         name="password"
         placeholder="password"
         type="password"
+        mb={2}
         onChange={onChange}
+        fontSize="10pt"
+        _placeholder={{ color: "gray.500" }}
+        _hover={{
+          bg: "white",
+          border: "1px solid",
+          borderColor: "blue.500",
+        }}
+        _focus={{
+          outline: "none",
+          bg: "white",
+          border: "1px solid",
+          borderColor: "blue.500",
+        }}
+        bg="gray.50"
       />
-      <Button type="submit">로그인</Button>
+      <Button width="100%" height="36px" mt={2} mb={2} type="submit">
+        로그인
+      </Button>
+      <Flex fontSize="9pt" justifyContent="center">
+        <Text mr={1} fontWeight={550}>
+          새로운 손님이세요? &nbsp;
+        </Text>
+        <Text
+          color="blue.500"
+          fontWeight={800}
+          cursor="pointer"
+          onClick={() =>
+            setAuthModalState((prev) => ({
+              ...prev,
+              view: "signup",
+            }))
+          }
+        >
+          회원가입
+        </Text>
+      </Flex>
     </form>
   );
 };
