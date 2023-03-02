047b9732a1e8ed798b86b354b05e838400c4ee97
inbum <inbum13@allisnull.com>
2023-03-02 13:50:21 +0900

    fix(.eslintrc.js): env 추가

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,9 +1,10 @@
 module.exports = {
   root: true,
+  env: {
+    browser: true,
+    es2022: true,
+  },
   parser: '@typescript-eslint/parser',
   plugins: ['@typescript-eslint'],
   extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended'],
-  env: {
-    browser: true,
-  },
 };
