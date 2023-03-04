f8123f59b466631c1e880226061854d57f5016f0
burger <fsm15@naver.com>
2023-03-04 18:24:44 +0900

    feat: firebase DB에서 community data를 가져오기

--- src/pages/r/[communityId]/index..tsx
+++ src/pages/r/[communityId]/index..tsx
@@ -1,4 +1,7 @@
 import React from 'react';
+import { GetServerSidePropsContext } from 'next';
+import { firestore } from '@/firebase/clientApp';
+import { doc, getDoc } from '@firebase/firestore';
 
 type CommunityPageProps = {};
 
@@ -9,5 +12,29 @@ const CommunityPage: React.FC<CommunityPageProps> = () => {
     </div>
   );
 };
+
+export async function getServerSideProps(context: GetServerSidePropsContext) {
+  // get community data and pass it to clinet => firebase db에 있는 실제 데이터를 가져오는 방법은 아래와 같다.
+  // 함수는 DocumentReference 인스턴스를 반환합니다.
+  // 이 인스턴스는 Firestore 데이터베이스에서 문서를 참조하는 데 사용됩니다.
+  // 반환된 DocumentReference 인스턴스는 firestore 인스턴스의 communities 컬렉션 내에서 context.query.communityId 값과 일치하는 문서를 참조합니다.
+  // 이 문서 참조를 사용하여 해당 문서의 데이터를 가져올 수 있습니다.
+  try {
+    const communityDocRef = doc(
+      firestore,
+      'communities',
+      context.query.communityId as string,
+    );
+    //getDoc() 함수를 사용하여 문서 참조에서 해당 문서의 데이터를 가져옵니다.
+    const communityDoc = await getDoc(communityDocRef);
+    return {
+      props: {
+        community: communityDoc.data(),
+      },
+    };
+  } catch (error) {
+    console.log(error);
+  }
+}
 
 export default CommunityPage;
