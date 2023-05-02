1d9ab180dd94c44b2f21a77d00b4e628bf02a056
burger <fsm15@naver.com>
2023-05-02 20:59:26 +0900

    feat: 에러핸들링

--- src/pages/r/[communityId]/index.tsx
+++ src/pages/r/[communityId]/index.tsx
@@ -28,11 +28,7 @@ export async function getServerSideProps(context: GetServerSidePropsContext) {
   // 반환된 DocumentReference 인스턴스는 firestore 인스턴스의 communities 컬렉션 내에서 context.query.communityId 값과 일치하는 문서를 참조합니다.
   // 이 문서 참조를 사용하여 해당 문서의 데이터를 가져올 수 있습니다.
   try {
-    const communityDocRef = doc(
-      firestore,
-      'communities',
-      context.query.communityId as string,
-    );
+    const communityDocRef = doc(firestore, 'communities', context.query.communityId as string);
     //getDoc() 함수를 사용하여 문서 참조에서 해당 문서의 데이터를 가져옵니다.
     const communityDoc = await getDoc(communityDocRef);
     return {
@@ -44,12 +40,19 @@ export async function getServerSideProps(context: GetServerSidePropsContext) {
                 ...communityDoc.data(),
               }),
             )
-          : null,
+          : {},
       },
     };
   } catch (error) {
     //CLOUD ADD ERROR PAGE HERE
+    // 아래 에러가 발생했기에 return을 해주었다.
+    // error - Error: Your `getServerSideProps` function did not return an object. Did you forget to add a `return`?
     console.log(`getServerSideprops error: ${error}`);
+    return {
+      props: {
+        communityData: {},
+      },
+    };
   }
 }
 
