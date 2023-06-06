88f741717ae421ff8b027ec91c7e6627ba9b5c33
burger <fsm15@naver.com>
2023-06-06 22:38:48 +0900

    add: custom hook 추가, communityState 추가

---
+++ src/hooks/useCommunityData.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+type UseCommunityDataProps = {
+  children: React.ReactNode;
+};
+
+const UseCommunityData: React.FC = () => {
+  return <div> </div>;
+};
+
+export default UseCommunityData;
