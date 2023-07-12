44913f8232e12ca2347e8da30fefbee353d93acb
burger <fsm15@naver.com>
2023-07-12 23:57:46 +0900

    feat: Image & video upload 기능 작업 완료 feat: post 생성 기능 완료 feat: post  생성 실패 시 에러 처리 완료

--- src/components/Posts/NewPostForm.tsx
+++ src/components/Posts/NewPostForm.tsx
@@ -1,5 +1,13 @@
 import React, { useState } from 'react';
-import { Flex, Icon } from '@chakra-ui/react';
+import {
+  Alert,
+  AlertDescription,
+  AlertIcon,
+  AlertTitle,
+  Flex,
+  Icon,
+  Text,
+} from '@chakra-ui/react';
 import { BiPoll } from 'react-icons/bi';
 import { IoImageOutline, IoDocumentText } from 'react-icons/io5';
 import { BsLink45Deg, BsMic } from 'react-icons/bs';
@@ -9,7 +17,15 @@ import ImageUpload from '@/components/Posts/PostForm/ImageUpload';
 import { Post } from '@/atoms/postsAtom';
 import { User } from 'firebase/auth';
 import { useRouter } from 'next/router';
-import { serverTimestamp, Timestamp } from '@firebase/firestore';
+import {
+  addDoc,
+  collection,
+  serverTimestamp,
+  Timestamp,
+  updateDoc,
+} from '@firebase/firestore';
+import { getDownloadURL, ref, uploadString } from '@firebase/storage';
+import { firestore, storage } from '@/firebase/clientApp';
 
 type NewPostFormProps = {
   user: User;
@@ -56,26 +72,46 @@ const NewPostForm: React.FC<NewPostFormProps> = ({ user }) => {
   });
   const [selectFile, setSelectedFile] = useState<string>();
   const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(false);
   const handleCreatePost = async () => {
     const { communityId } = router.query;
     //create new post object => type Post
     const newPost: Post = {
       communityId: communityId as string,
       creatorId: user?.uid,
-      creatorDisplayName: user.email!.split('@')[0],
+      creatorDisplayName: user.email?.split('@')[0],
       title: textInput.title,
       body: textInput.body,
       numberOfComments: 0,
       voteStatus: 0,
       createdAt: serverTimestamp() as Timestamp,
     };
-    console.log(newPost);
-    // store the post in db
-    //check for selectedFile
-    // store in storage => getDownloadURL (return imageURL)
-    // UPDATE POST DOC by adding imageURL
+
+    setLoading(true);
+    try {
+      // store the post in db
+      const postDocRef = await addDoc(collection(firestore, 'posts'), newPost);
+
+      //check for selectedFile
+      if (selectFile) {
+        // store in storage => getDownloadURL (return imageURL)
+        const imageRef = ref(storage, `posts/${postDocRef.id}/image`);
+        await uploadString(imageRef, selectFile, 'data_url');
+        const downloadURL = await getDownloadURL(imageRef);
+
+        // UPDATE POST DOC by adding imageURL
+        await updateDoc(postDocRef, {
+          imageURL: downloadURL,
+        });
+      }
+    } catch (e: any) {
+      console.log('handlecreatePost error', e.message);
+      setError(true);
+    }
+    setLoading(false);
 
     //redirect the user to the communityPage using the router
+    //    router.back();
   };
 
   const onSelectImage = (event: React.ChangeEvent<HTMLInputElement>) => {
@@ -134,6 +170,15 @@ const NewPostForm: React.FC<NewPostFormProps> = ({ user }) => {
           />
         )}
       </Flex>
+      {error && (
+        <Alert status='error'>
+          <AlertIcon />
+          <AlertTitle mr={2}>Error creating Post</AlertTitle>
+          <AlertDescription>
+            문제가 계속 발생하면 고객센터에 문의하세요.
+          </AlertDescription>
+        </Alert>
+      )}
     </Flex>
   );
 };
