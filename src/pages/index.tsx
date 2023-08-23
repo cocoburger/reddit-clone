a27bd9c80f7a3ba4e9ca36f1fc0a730ce78551b1
burger <fsm15@naver.com>
2023-08-23 23:38:09 +0900

    feature: 홈 페이지 작업

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,53 @@
 import { NextPage } from 'next';
+import PageContent from '@/components/Layout/PageContent';
+import { useAuthState } from 'react-firebase-hooks/auth';
+import { auth, firestore } from '@/firebase/clientApp';
+import { useEffect, useState } from 'react';
+import { useRecoilValue } from 'recoil';
+import { communityState } from '@/atoms/communitiesAtom';
+import {
+  collection,
+  getDocs,
+  limit,
+  orderBy,
+  query,
+} from '@firebase/firestore';
+import usePosts from '@/hooks/usePosts';
 
 const Home: NextPage = () => {
+  const [user, loadingUser] = useAuthState(auth);
+  const [loading, setLoading] = useState(false);
+  const { setPostStateValue } = usePosts();
+  const communityStateValue = useRecoilValue(communityState);
+
+  const buildUserHomeFeed = () => {};
+
+  const buildNoUserHomeFeed = async () => {
+    setLoading(true);
+    try {
+      const postQuery = query(
+        collection(firestore, 'posts'),
+        orderBy('voteStatus', 'desc'),
+        limit(10),
+      );
+      const postDocs = await getDocs(postQuery);
+      const posts = postDocs.docs.map((doc) => ({ id: doc.id, ...doc.data() }));
+    } catch (e) {
+      console.log('buildNoUserHomeFeed error : ', e);
+    }
+  };
+
+  const getUserPostVotes = () => {};
+
+  //useEffects
+  useEffect(() => {
+    if (!user && !loadingUser) buildNoUserHomeFeed();
+  }, [user, loadingUser]);
   return (
-    <div>
-      <h1>Hello</h1>
-    </div>
+    <PageContent>
+      <>{/*postFeed*/}</>
+      <>{/*recommendations*/}</>
+    </PageContent>
   );
 };
 export default Home;
